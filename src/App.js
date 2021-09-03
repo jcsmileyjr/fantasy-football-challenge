6b643c157b4ed968a9b33116ac0fe03d06f1e522
Smiley <jcsmileyjr@gmail.com>
2021-09-02 22:43:52 -0500

    Initial start of header and Title page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
       <header className="text-gray-900 text-center font-bold text-2xl">Fantasy Football Challenge</header>
+      <Title />
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+
+import BlueHelmets from '../assets/helmets-1-title-blue.png';
+
+const Title = () => {
+    return(
+        <article className="">
+            <header>
+                <img src={BlueHelmets} alt="football helmets" />
+            </header>
+            <section></section>
+        </article>
+    );
+}
+
+export default Title;
