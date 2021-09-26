fa29b6f5bcf804e1253dc3b1ce6cd22106d6223a
Smiley <jcsmileyjr@gmail.com>
2021-09-26 03:48:35 -0500

    Updated players UI to match White and Orange theme color change

--- src/components/Players.js
+++ src/components/Players.js
@@ -18,12 +18,12 @@ const Players = ({
   return (
     <article
       className={`flex-1 small-phone:w-8/12 w-5/12 mt-4 flex flex-col justify-between ${
-        drafted ? "bg-green-900 text-white" : " bg-white "
+        drafted ? "bg-yellow-600 text-black" : " bg-white "
       }`}
     >
       <section className="text-center mb-4">
         <strong className="text-xl sm:text-3xl small-phone:text-lg">{name}</strong>
-        <p className={`font-bold text-sm sm:text-2xl ${drafted?"text-yellow-500":"text-red-500"}`}>{role}</p>
+        <p className={`font-bold text-sm sm:text-2xl ${drafted?"text-white":"text-red-500"}`}>{role}</p>
       </section>
       <section className="text-xs flex flex-col justify-between mx-2 sm:mx-auto md:w-10/12 lg:w-2/3">
         <div className="flex flex-row justify-between sm:text-2xl">
@@ -40,7 +40,7 @@ const Players = ({
             type="button"
             className={` text-sm mb-2 py-1 px-4 md:px-8 md:py-2 rounded-lg md:font-bold md:text-2xl ${
               drafted
-                ? "bg-white text-green-900 font-bold"
+                ? "bg-white text-blue-900 font-bold"
                 : "bg-gray-500 text-white"
             }`}
             onClick={() => {
